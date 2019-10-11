48754dd052d5deb8224d097dc86897d7e628cb37
quangtung97 <quangtung29121997@gmail.com>
2019-10-11 13:04:32 +0700

    Add WebRTC

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,7 +1,7 @@
 import "../css/app.css";
 import "phoenix_html";
 import "./socket.ts";
-import {Socket} from "phoenix";
+import { Socket } from "phoenix";
 import LiveSocket from "phoenix_live_view";
 
 const liveSocket = new LiveSocket("/live", Socket);
